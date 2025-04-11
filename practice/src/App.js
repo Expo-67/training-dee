d46d1660d3881b540e13e8fde8b777601dc1dd72
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2025-04-11 12:38:17 +0300

    first commit

---
+++ practice/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1> The Avatar </h1>
+      <div>
+        <button> Kiyoshi</button> <button> Aang</button> <button>Roku </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
